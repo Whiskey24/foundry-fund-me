689e7258c0d02c60a9f2051e38c8e06c1e452de2
Whiskey24 <bert@santema.eu>
2025-08-26 22:49:17 +0200

    added tests (up to L10)

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumUSDIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.I_OWNER(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        assertEq(fundMe.getVersion(), 4);
+    }
+
+    // function testFundFailsWithoutEnoughETH() public {
+    //     vm.expectRevert();
+    //     fundMe.fund();
+    // }
+
+    // function testFundUpdatesFundedDataStructure() public {
+    //     vm.prank(USER);
+    //     fundMe.fund{value: SEND_VALUE}();
+    //     uint256 amountFunded = fundMe.addressToAmountFunded(USER);
+    //     assertEq(amountFunded, SEND_VALUE);
+    // }
+}
